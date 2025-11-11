37e78d70821f080d32ca5be3fdf677c24561dc3e
ItzSrujan <mr.morningstar246@gmail.com>
2025-11-11 17:39:12 +0530

    frontend issue resolved

--- agrosathi/src/components/ImageUpload.js
+++ agrosathi/src/components/ImageUpload.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./ImageUpload.css";
-// import translate from "translate";
+import translate from "translate";
 
 const ImageUpload = () => {
   const [image, setImage] = useState(null);
